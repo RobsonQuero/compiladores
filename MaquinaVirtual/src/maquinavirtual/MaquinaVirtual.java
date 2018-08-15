dec9c5c4286ce9ec82e01f8b7dbce419c7f5e8cf
felipeviveiros <felipeviveiros@hotmail.com>
2018-08-15 14:25:51 -0300

    Add files via upload

---
+++ MaquinaVirtual/src/maquinavirtual/MaquinaVirtual.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maquinavirtual;
+
+/**
+ *
+ * @author felipe
+ */
+public class MaquinaVirtual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new tela().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
